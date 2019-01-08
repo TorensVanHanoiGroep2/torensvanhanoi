4b78030f75089b58db08de28a073ce016ca0a859
Sem van Elteren <sem407@outlook.com>
2019-01-08 16:39:42 +0100

    added recursion and time indicator

--- Torens van Hanoi Groep 2/Form1.Designer.cs
+++ Torens van Hanoi Groep 2/Form1.Designer.cs
@@ -38,8 +38,9 @@
             this.machtsverheffingLabel = new System.Windows.Forms.Label();
             this.incursieGroupBox = new System.Windows.Forms.GroupBox();
             this.RecursieTextBox = new System.Windows.Forms.TextBox();
-            this.IncursieButton = new System.Windows.Forms.Button();
-            this.incursieLabel = new System.Windows.Forms.Label();
+            this.recursieButton = new System.Windows.Forms.Button();
+            this.recursieLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
             this.forLoopGroupBox.SuspendLayout();
             this.machtsverheffingGroupBox.SuspendLayout();
             this.incursieGroupBox.SuspendLayout();
@@ -68,7 +69,7 @@
             // 
             this.forLoopLabel.AutoSize = true;
             this.forLoopLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.forLoopLabel.Location = new System.Drawing.Point(103, 89);
+            this.forLoopLabel.Location = new System.Drawing.Point(42, 89);
             this.forLoopLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.forLoopLabel.Name = "forLoopLabel";
             this.forLoopLabel.Size = new System.Drawing.Size(84, 25);
@@ -84,7 +85,7 @@
             this.forLoopGroupBox.Margin = new System.Windows.Forms.Padding(4);
             this.forLoopGroupBox.Name = "forLoopGroupBox";
             this.forLoopGroupBox.Padding = new System.Windows.Forms.Padding(4);
-            this.forLoopGroupBox.Size = new System.Drawing.Size(293, 214);
+            this.forLoopGroupBox.Size = new System.Drawing.Size(358, 214);
             this.forLoopGroupBox.TabIndex = 16;
             this.forLoopGroupBox.TabStop = false;
             this.forLoopGroupBox.Text = "for loop";
@@ -94,11 +95,11 @@
             this.machtsverheffingGroupBox.Controls.Add(this.machtsverheffingTextBox);
             this.machtsverheffingGroupBox.Controls.Add(this.machtsverheffingButton);
             this.machtsverheffingGroupBox.Controls.Add(this.machtsverheffingLabel);
-            this.machtsverheffingGroupBox.Location = new System.Drawing.Point(317, 53);
+            this.machtsverheffingGroupBox.Location = new System.Drawing.Point(399, 53);
             this.machtsverheffingGroupBox.Margin = new System.Windows.Forms.Padding(4);
             this.machtsverheffingGroupBox.Name = "machtsverheffingGroupBox";
             this.machtsverheffingGroupBox.Padding = new System.Windows.Forms.Padding(4);
-            this.machtsverheffingGroupBox.Size = new System.Drawing.Size(293, 214);
+            this.machtsverheffingGroupBox.Size = new System.Drawing.Size(337, 214);
             this.machtsverheffingGroupBox.TabIndex = 17;
             this.machtsverheffingGroupBox.TabStop = false;
             this.machtsverheffingGroupBox.Text = "machtsverheffing";
@@ -126,7 +127,7 @@
             // 
             this.machtsverheffingLabel.AutoSize = true;
             this.machtsverheffingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.machtsverheffingLabel.Location = new System.Drawing.Point(107, 89);
+            this.machtsverheffingLabel.Location = new System.Drawing.Point(46, 89);
             this.machtsverheffingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.machtsverheffingLabel.Name = "machtsverheffingLabel";
             this.machtsverheffingLabel.Size = new System.Drawing.Size(84, 25);
@@ -136,16 +137,16 @@
             // incursieGroupBox
             // 
             this.incursieGroupBox.Controls.Add(this.RecursieTextBox);
-            this.incursieGroupBox.Controls.Add(this.IncursieButton);
-            this.incursieGroupBox.Controls.Add(this.incursieLabel);
-            this.incursieGroupBox.Location = new System.Drawing.Point(619, 53);
+            this.incursieGroupBox.Controls.Add(this.recursieButton);
+            this.incursieGroupBox.Controls.Add(this.recursieLabel);
+            this.incursieGroupBox.Location = new System.Drawing.Point(760, 53);
             this.incursieGroupBox.Margin = new System.Windows.Forms.Padding(4);
             this.incursieGroupBox.Name = "incursieGroupBox";
             this.incursieGroupBox.Padding = new System.Windows.Forms.Padding(4);
-            this.incursieGroupBox.Size = new System.Drawing.Size(293, 214);
+            this.incursieGroupBox.Size = new System.Drawing.Size(327, 214);
             this.incursieGroupBox.TabIndex = 18;
             this.incursieGroupBox.TabStop = false;
-            this.incursieGroupBox.Text = "incursie";
+            this.incursieGroupBox.Text = "recursie";
             // 
             // RecursieTextBox
             // 
@@ -155,32 +156,43 @@
             this.RecursieTextBox.Size = new System.Drawing.Size(200, 22);
             this.RecursieTextBox.TabIndex = 6;
             // 
-            // IncursieButton
+            // recursieButton
             // 
-            this.IncursieButton.Location = new System.Drawing.Point(51, 139);
-            this.IncursieButton.Margin = new System.Windows.Forms.Padding(4);
-            this.IncursieButton.Name = "IncursieButton";
-            this.IncursieButton.Size = new System.Drawing.Size(201, 48);
-            this.IncursieButton.TabIndex = 0;
-            this.IncursieButton.Text = "Bereken";
-            this.IncursieButton.UseVisualStyleBackColor = true;
+            this.recursieButton.Location = new System.Drawing.Point(51, 139);
+            this.recursieButton.Margin = new System.Windows.Forms.Padding(4);
+            this.recursieButton.Name = "recursieButton";
+            this.recursieButton.Size = new System.Drawing.Size(201, 48);
+            this.recursieButton.TabIndex = 0;
+            this.recursieButton.Text = "Bereken";
+            this.recursieButton.UseVisualStyleBackColor = true;
+            this.recursieButton.Click += new System.EventHandler(this.recursieButton_Click);
             // 
-            // incursieLabel
+            // recursieLabel
             // 
-            this.incursieLabel.AutoSize = true;
-            this.incursieLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.incursieLabel.Location = new System.Drawing.Point(107, 89);
-            this.incursieLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.incursieLabel.Name = "incursieLabel";
-            this.incursieLabel.Size = new System.Drawing.Size(84, 25);
-            this.incursieLabel.TabIndex = 13;
-            this.incursieLabel.Text = "uitkomst";
+            this.recursieLabel.AutoSize = true;
+            this.recursieLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recursieLabel.Location = new System.Drawing.Point(46, 89);
+            this.recursieLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.recursieLabel.Name = "recursieLabel";
+            this.recursieLabel.Size = new System.Drawing.Size(84, 25);
+            this.recursieLabel.TabIndex = 13;
+            this.recursieLabel.Text = "uitkomst";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(13, 302);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 17);
+            this.timeLabel.TabIndex = 19;
+            
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(947, 318);
+            this.ClientSize = new System.Drawing.Size(1093, 349);
+            this.Controls.Add(this.timeLabel);
             this.Controls.Add(this.incursieGroupBox);
             this.Controls.Add(this.machtsverheffingGroupBox);
             this.Controls.Add(this.forLoopGroupBox);
@@ -195,6 +207,7 @@
             this.incursieGroupBox.ResumeLayout(false);
             this.incursieGroupBox.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -210,8 +223,9 @@
         private System.Windows.Forms.Label machtsverheffingLabel;
         private System.Windows.Forms.GroupBox incursieGroupBox;
         private System.Windows.Forms.TextBox RecursieTextBox;
-        private System.Windows.Forms.Button IncursieButton;
-        private System.Windows.Forms.Label incursieLabel;
+        private System.Windows.Forms.Button recursieButton;
+        private System.Windows.Forms.Label recursieLabel;
+        private System.Windows.Forms.Label timeLabel;
     }
 }
 
